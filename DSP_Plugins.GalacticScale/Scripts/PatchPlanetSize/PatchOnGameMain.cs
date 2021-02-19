080e7e3c03ecaec6b0774cec6893beea99111cca
Touhma <>
2021-02-19 15:06:02 -0500

    fix for terrain on loading

---
+++ DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchOnGameMain.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace GalacticScale.Scripts.PatchPlanetSize {
+    [HarmonyPatch(typeof(GameMain))]
+    public class PatchOnGameMain {
+        [HarmonyPostfix]
+        [HarmonyPatch("Begin")]
+        public static void Begin(GameMain __instance) {
+            Debug.Log("PatchOnGameMain Postfix ?");
+            if (GameMain.isRunning) {
+                if (GameMain.localPlanet != null && GameMain.mainPlayer != null) {
+                    GameMain.localPlanet.factory.FlattenTerrain(GameMain.mainPlayer.position, GameMain.mainPlayer.uRotation,
+                        new Bounds(Vector3.zero, new Vector3(1f, 1f, 1f)), removeVein: false, lift: false);
+                    GameMain.localPlanet.factory.RenderLocalPlanetHeightmap();
+                }
+            }
+        }
+    }
+}
