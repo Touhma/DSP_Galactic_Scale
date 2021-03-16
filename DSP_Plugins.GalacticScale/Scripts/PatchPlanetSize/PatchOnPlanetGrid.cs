d149d13201d541af2c30abda68a2fbccb860c957
innominata <inno@xray.cat>
2021-03-16 22:16:19 +1300

    Update PatchOnPlanetGrid.cs

--- DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchOnPlanetGrid.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchOnPlanetGrid.cs
@@ -13,26 +13,27 @@ namespace GalacticScale.Scripts.PatchPlanetSize {
         [HarmonyPrefix]
         [HarmonyPatch("DetermineLongitudeSegmentCount")]
         public static bool DetermineLongitudeSegmentCount(int latitudeIndex, int segment, ref int __result) {
-            if (Patch.EnableResizingFeature.Value || Patch.EnableLimitedResizingFeature.Value) {
-                Patch.Debug("PlanetGrid Vanilla DetermineLongitudeSegmentCount.", LogLevel.Debug, Patch.DebugNewPlanetGrid);
-                if (keyedLUTs.ContainsKey(segment)) {
-                    Patch.Debug("PlanetGrid Vanilla DeterminLongitudeSegmentCount Key Existed.", LogLevel.Debug, Patch.DebugNewPlanetGrid);
-                    int index = Mathf.Abs(latitudeIndex) % (segment / 2);
-                    if (index >= segment / 4) {
-                        index = segment / 4 - index;
+            if (!DSPGame.IsMenuDemo) 
+                if (Patch.EnableResizingFeature.Value || Patch.EnableLimitedResizingFeature.Value) {
+                    Patch.Debug("PlanetGrid Vanilla DetermineLongitudeSegmentCount.", LogLevel.Debug, Patch.DebugNewPlanetGrid);
+                    if (keyedLUTs.ContainsKey(segment)) {
+                        Patch.Debug("PlanetGrid Vanilla DeterminLongitudeSegmentCount Key Existed.", LogLevel.Debug, Patch.DebugNewPlanetGrid);
+                        int index = Mathf.Abs(latitudeIndex) % (segment / 2);
+                        if (index >= segment / 4) {
+                            index = segment / 4 - index;
+                        }
+
+                        Patch.Debug("PlatformSystem Vanilla DetermineLongitudeSegmentCount fetched " + keyedLUTs[segment][index] + " for segments " + segment + " at index " + latitudeIndex + "(" + index + ")", LogLevel.Debug, Patch.DebugNewPlanetGrid);
+                        __result = keyedLUTs[segment][index];
+                    }
+                    else {
+                        //Original algorithm. Really shouldn't be used anymore... but just in case it's still here.
+                        var index = Mathf.CeilToInt(Mathf.Abs(Mathf.Cos((float) (latitudeIndex / (double) (segment / 4f) * 3.14159274101257 * 0.5))) * segment);
+                        __result = index < 500 ? PlatformSystem.segmentTable[index] : (index + 49) / 100 * 100;
                     }
 
-                    Patch.Debug("PlatformSystem Vanilla DetermineLongitudeSegmentCount fetched " + keyedLUTs[segment][index] + " for segments " + segment + " at index " + latitudeIndex + "(" + index + ")", LogLevel.Debug, Patch.DebugNewPlanetGrid);
-                    __result = keyedLUTs[segment][index];
+                    return false;
                 }
-                else {
-                    //Original algorithm. Really shouldn't be used anymore... but just in case it's still here.
-                    var index = Mathf.CeilToInt(Mathf.Abs(Mathf.Cos((float) (latitudeIndex / (double) (segment / 4f) * 3.14159274101257 * 0.5))) * segment);
-                    __result = index < 500 ? PlatformSystem.segmentTable[index] : (index + 49) / 100 * 100;
-                }
-
-                return false;
-            }
             return true;
         }
 
