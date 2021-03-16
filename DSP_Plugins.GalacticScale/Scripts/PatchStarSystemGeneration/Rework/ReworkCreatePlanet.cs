c377e7ba4c2dbade3dfccdfdf2c49dc4e60db326
innominata <inno@xray.cat>
2021-03-17 00:15:24 +1300

    Update ReworkCreatePlanet.cs
    Whoops, that was already fixed

--- DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/Rework/ReworkCreatePlanet.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/Rework/ReworkCreatePlanet.cs
@@ -408,7 +408,7 @@ namespace GalacticScale.Scripts.PatchStarSystemGeneration {
                                     }
                                 }
                             }
-                            break;
+                            
                         }
                     }
                 }
