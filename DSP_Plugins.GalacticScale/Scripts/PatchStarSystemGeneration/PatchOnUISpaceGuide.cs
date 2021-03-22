ba6a7b4929752b04a4e175c8c843bb2ac8fc8a08
Charlie Brensinger <cbrensinger@glitchhound.com>
2021-03-21 21:43:36 -0400

    Fix UISpaceGuide for patch 0.6.17.5972
    Check the vanilla 10 times when outside of any particular star system,
    but check planetCount times whenever in a particular star system.
    
    Convert to delegate; much easier to follow than CIL translation of it.

--- DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/PatchOnUISpaceGuide.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/PatchOnUISpaceGuide.cs
@@ -30,19 +30,24 @@ namespace GalacticScale.Scripts.PatchStarSystemGeneration {
             return ReplaceLd10(instructions);
         }
 
-        public static IEnumerable<CodeInstruction> ReplaceLd10(IEnumerable<CodeInstruction> instructions) {
+        public static IEnumerable<CodeInstruction> ReplaceLd10(IEnumerable<CodeInstruction> instructions)
+        {
             var codes = new List<CodeInstruction>(instructions);
-            for (var i = 0; i < codes.Count; i++)
-                if (codes[i].opcode == OpCodes.Ldc_I4_S && codes[i].OperandIs(10)) {
-                    var newInstructions = new List<CodeInstruction>();
-                    newInstructions.Add(new CodeInstruction(OpCodes.Ldarg_0));// this.
-                    newInstructions.Add(new CodeInstruction(OpCodes.Ldfld, typeof(UISpaceGuide).GetField("gameData")));// gameData.
-                    newInstructions.Add(new CodeInstruction(OpCodes.Callvirt, typeof(GameData).GetMethod("get_localStar")));// localStar.
-                    newInstructions.Add(new CodeInstruction(OpCodes.Ldfld, typeof(StarData).GetField("planetCount")));// planetCount
-                    codes.RemoveAt(i);// remove the original instruction to load 10
-                    codes.InsertRange(i, newInstructions);// in its place, instead load the combined lookup: this.gameData.localStar.planetCount
+            for (var i = 0; i < codes.Count; i++) { 
+                if (codes[i].opcode == OpCodes.Ldc_I4_S && codes[i].OperandIs(10))
+                {
+                    codes[i] = new CodeInstruction(Transpilers.EmitDelegate<Del>( // replace load10 with this delegate
+                    () =>
+                    {
+                        // If localStar is defined, use its planetCount
+                        if (GameMain.localStar != null) return GameMain.localStar.planetCount;
+                        // If localStar is not defined, stick with the default 10
+                        return 10;
+                    }));
                 }
+            }
             return codes.AsEnumerable();
         }
+        delegate int Del();
     }
 }
