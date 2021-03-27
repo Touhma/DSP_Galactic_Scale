8dbc5b6670e5d87912f5fdcc83a052c9d0cb65e0
innominata <inno@xray.cat>
2021-03-28 00:13:43 +1300

    Update ReworkCreatePlanet.cs
    We were using the same random number for all the checks, which are independent variables. Didn't work

--- DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/Rework/ReworkCreatePlanet.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/Rework/ReworkCreatePlanet.cs
@@ -114,13 +114,13 @@ namespace GalacticScale.Scripts.PatchStarSystemGeneration {
                 planetData.sunDistance = planetData.orbitRadius;
 
                 //Tidal Lock Management
-                if (randomNumber13 < Patch.ChanceTidalLock.Value) {
+                if (randomNumber12 < Patch.ChanceTidalLock.Value) {
                     if (randomNumber13 < Patch.ChanceTidalLock1.Value) {
                         planetData.obliquity *= 0.01f;
                         planetData.rotationPeriod = planetData.orbitalPeriod;
                         planetData.IsTidallyLocked(TidalLevel.TidalLocked);
                     }
-                    else if (randomNumber13 < Patch.ChanceTidalLock2.Value) {
+                    else if (randomNumber7 < Patch.ChanceTidalLock2.Value) {
                         planetData.obliquity *= 0.1f;
                         planetData.rotationPeriod = planetData.orbitalPeriod * 0.5;
                         planetData.IsTidallyLocked(TidalLevel.TidalLocked2);
