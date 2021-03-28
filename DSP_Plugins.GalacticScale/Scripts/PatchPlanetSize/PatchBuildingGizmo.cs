8b818c4946feb274cbb0e576adbdfaad662a005a
innominata <inno@xray.cat>
2021-03-29 11:12:02 +1300

    Tidy

--- DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchBuildingGizmo.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchBuildingGizmo.cs
@@ -12,15 +12,12 @@ namespace GalacticScale.Scripts.PatchPlanetSize {
     {
         [HarmonyPostfix]
         [HarmonyPatch("SetGizmoDesc")]
-        public static void PatchSetGizmoDesc(BuildGizmoDesc _desc, Transform ___minerFan)
+        public static void SetGizmoDesc(BuildGizmoDesc _desc, Transform ___minerFan)
         {
             if (_desc.desc.minerType == EMinerType.Vein)
             {
                 if (GameMain.localPlanet == null) return;
-                //float angle = Mathf.Rad2Deg * (-1.0f * Mathf.Abs((Mathf.PI / 200) - Mathf.PI / ((GameMain.localPlanet.radius / 4.0f) * (int)4))); // Hopefully get difference in tangent in radians and turns into degrees
                 float angle = ((200 / GameMain.localPlanet.radius) * Mathf.PI - Mathf.PI);
-                //Quaternion q = Quaternion.Euler(0, -10.0f * angle, 0);
-                //Patch.Log(angle);
                 ___minerFan.localEulerAngles = new Vector3(angle, 180f, 0f);
             }
         }
