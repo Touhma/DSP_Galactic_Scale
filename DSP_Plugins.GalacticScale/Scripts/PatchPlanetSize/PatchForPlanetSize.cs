d49fe838d7728cf565323d8874e8309a798f40e7
innominata <inno@xray.cat>
2021-03-29 11:32:46 +1300

    Tidy

--- DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchForPlanetSize.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchForPlanetSize.cs
@@ -141,8 +141,6 @@ namespace GalacticScale.Scripts.PatchPlanetSize {
                 Harmony.CreateAndPatchAll(typeof(PatchOnGameMain));
                 Harmony.CreateAndPatchAll(typeof(PatchBuildingGizmo));
                 Harmony.CreateAndPatchAll(typeof(PatchNearColliderLogic));
-                Harmony.CreateAndPatchAll(typeof(PatchUIPlanetGlobe));
-                Harmony.CreateAndPatchAll(typeof(PatchUIGameLoadingSplash));
                 Harmony.CreateAndPatchAll(typeof(PatchUI));
             }
         }
@@ -161,35 +159,5 @@ namespace GalacticScale.Scripts.PatchPlanetSize {
         public static void Debug(object data, LogLevel logLevel, bool isActive) {
             if (isActive) Logger.Log(logLevel, data);
         }
-        public static void Log(object data) //Leaving here because the other one is a pain to type when you're rapidly debugging using the manual log method...
-        {
-            Debug(data, LogLevel.Message, true);
-        }
-
-        //This library is amazing for debugging.
-        //private static readonly fsSerializer _serializer = new fsSerializer();
-
-        //public static string Serialize(Type type, object value)
-        //{
-        //    // serialize the data
-        //    fsData data;
-        //    _serializer.TrySerialize(type, value, out data).AssertSuccessWithoutWarnings();
-
-        //    // emit the data via JSON
-        //    return fsJsonPrinter.CompressedJson(data);
-        //}
-
-        //public static object Deserialize(Type type, string serializedState)
-        //{
-        //    // step 1: parse the JSON data
-        //    fsData data = fsJsonParser.Parse(serializedState);
-
-        //    // step 2: deserialize the data
-        //    object deserialized = null;
-        //    _serializer.TryDeserialize(data, type, ref deserialized).AssertSuccessWithoutWarnings();
-
-        //    return deserialized;
-        //}
-
     }
 }
--- DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchUI.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchUI.cs
@@ -4,14 +4,51 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.Rendering;
 using Patch = GalacticScale.Scripts.PatchPlanetSize.PatchForPlanetSize;
+using System.Reflection;
+using System.IO;
+using UnityEngine;
 
 namespace GalacticScale.Scripts.PatchPlanetSize {
-    [HarmonyPatch(typeof(UIVersionText))]
     public class PatchUI{
+        [HarmonyPostfix, HarmonyPatch(typeof(UIPlanetGlobe), "_OnUpdate")]
+        public static void PatchOnUpdate(ref Text ___geoInfoText)
+        {
+            if (GameMain.localPlanet != null && VFInput.alt) ___geoInfoText.text = "\r\nRadius " + GameMain.localPlanet.radius;
+        }
 
-        [HarmonyPrefix]
-        [HarmonyPatch("Refresh")]
-        public static bool PatchRefresh (ref Text ___textComp, string ___prefix, ref AccountData ___displayAccount, ref bool ___firstFrame)
+        [HarmonyPostfix, HarmonyPatch(typeof(UIGameLoadingSplash), "OnEnable")]
+        public static void OnEnable(ref Text ___promptText, ref RawImage ___noiseImage1)
+        {
+            ___promptText.text = "WARNING - Galactic Scale savegames can be broken by updates. Read the FAQ @ http://customizing.space";
+            string dir = Path.GetDirectoryName(Assembly.GetAssembly(typeof(PatchUIGameLoadingSplash)).Location) + "\\splash.jpg";
+            Patch.Log(dir);
+            Texture2D tex = null;
+            byte[] fileData;
+            if (File.Exists(dir))
+            {
+                fileData = File.ReadAllBytes(dir);
+                tex = new Texture2D(2, 2);
+                ImageConversion.LoadImage(tex, fileData);
+            }
+            Image[] images = UIRoot.instance.overlayCanvas.GetComponentsInChildren<Image>();
+            RawImage[] rimages = UIRoot.instance.overlayCanvas.GetComponentsInChildren<RawImage>();
+            foreach (Image image in images)
+            {
+                if (image.name == "black-bg")
+                {
+                    image.sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0, 0), 100f);
+                    image.color = Color.white;
+                }
+                else if (image.name == "bg" || image.name == "dots" || image.name == "dsp") image.enabled = false;
+            }
+            foreach (RawImage rimage in rimages)
+            {
+                if (rimage.name == "vignette") rimage.enabled = false;
+            }
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(UIVersionText), "Refresh")]
+        public static bool Refresh (ref Text ___textComp, string ___prefix, ref AccountData ___displayAccount, ref bool ___firstFrame)
         {
             if ((UnityEngine.Object)___textComp != (UnityEngine.Object)null)
             {
