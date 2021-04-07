eaf4ae3803c1aba0b87ff2dce068e8279f62a23e
innominata <inno@xray.cat>
2021-04-07 19:29:15 +1200

    test

--- DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchOnGameMain.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchPlanetSize/PatchOnGameMain.cs
@@ -6,6 +6,7 @@ namespace GalacticScale.Scripts.PatchPlanetSize {
     [HarmonyPatch(typeof(GameMain))]
     public class PatchOnGameMain
     {
+        static int delay = 0;
         [HarmonyPrefix]
         [HarmonyPatch("OnMainCameraPostRender")]
         static bool PatchOnMainCameraPostRender(Camera cam) 
@@ -17,20 +18,24 @@ namespace GalacticScale.Scripts.PatchPlanetSize {
         [HarmonyPostfix, HarmonyPatch("FixedUpdate")]
         static void DebugCommand()
         {
-            if (VFInput.alt && Input.GetKeyDown(KeyCode.D))
+            if (VFInput.alt && Input.GetKeyDown(KeyCode.D) && delay == 0)
             {
                 PlanetData planet = GameMain.localPlanet;
                 Vector3 position = GameMain.mainPlayer.position;
                 Patch.Debug("DebugCommand", BepInEx.Logging.LogLevel.Message, true);
                 Patch.Debug("---------------", BepInEx.Logging.LogLevel.Message, true);
-                Patch.Debug("Planet Segment Division: " + ((int)(planet.radius / 4f + 0.1f) * 4), BepInEx.Logging.LogLevel.Message, true);
-                Patch.Debug("Planet Segments: " + planet.segment, BepInEx.Logging.LogLevel.Message, true);
+                Patch.Debug("Planet Segments : " + ((int)(planet.radius / 4f + 0.1f) * 4), BepInEx.Logging.LogLevel.Message, true);
+                Patch.Debug("Planet Segment Division: " + planet.segment, BepInEx.Logging.LogLevel.Message, true);
                 Patch.Debug("Position: " + position, BepInEx.Logging.LogLevel.Message, true);
                 Patch.Debug("Height: " + planet.data.QueryHeight(position), BepInEx.Logging.LogLevel.Message, true);
                 Patch.Debug("ModifiedHeight: " + planet.data.QueryModifiedHeight(position), BepInEx.Logging.LogLevel.Message, true);
-                Patch.Debug("Last 2 ModData's: " + planet.data.modData[planet.data.modData.Length -2] + ", " + planet.data.modData[planet.data.modData.Length - 2], BepInEx.Logging.LogLevel.Message, true);
+                Patch.Debug("Position Hash: " + planet.data.PositionHash(position), BepInEx.Logging.LogLevel.Message, true);
+                Patch.Debug("Precision: " + planet.data.precision, BepInEx.Logging.LogLevel.Message, true);
+                Patch.Debug("Algorithm: " + planet.algoId, BepInEx.Logging.LogLevel.Message, true);
                 Patch.Debug("---------------", BepInEx.Logging.LogLevel.Message, true);
+                delay = 5;
             }
+            else if (delay > 0) delay--;
         }
     }
 }
