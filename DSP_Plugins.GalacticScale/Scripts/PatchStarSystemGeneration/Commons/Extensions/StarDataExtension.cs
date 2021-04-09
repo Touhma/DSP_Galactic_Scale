7f91f9c39db78d6745764206278bf4fd84bd97e3
Touhma <>
2021-04-08 23:57:53 -0400

    Fixing V2.0

--- DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/Commons/Extensions/StarDataExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GalacticScale.Scripts {
-    public static class StarDataExtension {
-        public static bool IsStartingStar(this StarData star) {
-            return star.index == 0;
-        }
-        
-    }
-}
