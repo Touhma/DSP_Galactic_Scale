766cec2045a990d84f9950e821969f8e130782c0
Touhma <>
2021-04-08 23:39:13 -0400

    restoration of the v3

---
+++ DSP_Plugins.GalacticScale/Scripts/Commons/Model/Generator/Settings/GeneratorStarSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GalacticScale.Scripts {
+    public class GeneratorStarSettings {
+        public int MinTemperature = 0;
+        public int MaxTemperature = 0;
+        
+        public float MinMass = 0f;
+        public float MaxMass = 0f;
+        
+        public float MinRadius = 0f;
+        public float MaxRadius = 0f;
+        
+        public float MinLuminosity = 0f;
+        public float MaxLuminosity = 0f; 
+        
+        public float MinHabitableRadius = 0f;
+        public float MaxHabitableRadius = 0f;
+        
+        public int MinAge = 0;
+        public int MaxAge = 0;
+    }
+}
---
+++ DSP_Plugins.GalacticScale/Scripts/Commons/Model/PlanetTheme.cs
@@ -0,0 +1,21 @@
+﻿namespace GalacticScale.Scripts {
+    public enum PlanetTheme {
+        None = 0,
+        Birth = 1,
+        RedGasGiant = 2,
+        YellowGasGiant = 3,
+        GreenIceGiant = 4,
+        BlueIceGiant = 5,
+        AridDesert = 6,
+        Gelisol = 7,
+        OceanicJungle = 8,
+        Lava = 9,
+        IceFieldGelisol = 10,
+        BarrenDesert = 11,
+        Gobi = 12,
+        VolcanicAsh = 13,
+        RedStone = 14,
+        Prairie = 15,
+        Ocean = 16,
+    }
+}
