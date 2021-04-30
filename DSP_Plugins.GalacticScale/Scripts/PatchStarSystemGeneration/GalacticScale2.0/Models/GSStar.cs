6a79b9d82b05188c39f9e18b3fe7c76a2d7cda5f
innominata <inno@xray.cat>
2021-04-30 18:31:10 +1200

    Update GSStar.cs

--- DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/GalacticScale2.0/Models/GSStar.cs
+++ DSP_Plugins.GalacticScale/Scripts/PatchStarSystemGeneration/GalacticScale2.0/Models/GSStar.cs
@@ -86,7 +86,7 @@ namespace GalacticScale
         [SerializeField]
         public float acDiscRadius { get => _acdiscRadius < 0 ? getAcDiscRadius() : _acdiscRadius; set => _acdiscRadius = value; }
         [SerializeField]
-        public VectorLF3 position { get => (_pos == null || (_pos == new VectorLF3() && assignedIndex != 0)) ? getPos() : _pos; set => _pos = value; }
+        public VectorLF3 position { get => (_pos == null ) ? getPos() : _pos; set => _pos = value; }//|| (_pos == new VectorLF3() && assignedIndex != 0)
         [NonSerialized]
         public int assignedIndex = 0;
 
