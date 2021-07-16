63095ed8f6938f9702bb5ed5dfbecec073641a60
innominata <inno@xray.cat>
2021-07-16 15:03:37 +1200

    b73 Fixed Station Build connections

--- DSP_Plugins.GalacticScale/Scripts/GalacticScale2.0/Patches/BuildTool_Path/CheckBuildConditions.cs
+++ DSP_Plugins.GalacticScale/Scripts/GalacticScale2.0/Patches/BuildTool_Path/CheckBuildConditions.cs
@@ -16,42 +16,50 @@ namespace GalacticScale
             }
 
             BuildPreview preview = __instance.buildPreviews[0];
+            BuildPreview preview2 = __instance.buildPreviews[__instance.buildPreviews.Count - 1];
             int objId = preview.inputObjId;
+            int objId2 = preview2.outputObjId;
             //GS2.Warn(objId.ToString());
             PlanetFactory ___factory = __instance.factory;
-            if (objId < 0 || objId >= ___factory.entityPool.Length)
+            if ((objId < 0 && objId2 < 0) || (objId >= ___factory.entityPool.Length && objId2 >= ___factory.entityPool.Length))
             {
                 return __result; // Sanity Check
             }
 
             EntityData entity = ___factory.entityPool[objId];
-            if (entity.isNull)
+            EntityData entity2 = ___factory.entityPool[objId2];
+            if (entity.isNull && entity2.isNull)
             {
                 return __result;
             }
 
             ItemProto itemProto = LDB.items.Select(entity.protoId); // Grab the prototype of the first object in the chain
-            if (itemProto == null)
+            ItemProto itemProto2 = LDB.items.Select(entity2.protoId);
+            if (itemProto == null && itemProto2 == null)
             {
                 return __result;
             }
 
-            if (itemProto.prefabDesc == null)
+            if (itemProto?.prefabDesc == null && itemProto2?.prefabDesc == null)
             {
                 return __result;
             }
-
-            if (itemProto.prefabDesc.oilMiner || itemProto.prefabDesc.veinMiner) // Check that we are connected to an oil miner
+            //GS2.Log($"{itemProto?.prefabDesc?.isStation} | {itemProto2?.prefabDesc?.isStation}");
+            //GS2.Log($"{itemProto2?.Name}");
+            if ((itemProto != null && itemProto.prefabDesc.oilMiner) || (itemProto != null && itemProto.prefabDesc.veinMiner) || (itemProto != null && itemProto.prefabDesc.isStation) || (itemProto2 != null && itemProto2.prefabDesc.isStation)) // Check that we are connected to an oil miner
             {
-                if (preview.condition == EBuildCondition.JointCannotLift) // Make sure the error is that the endpoint must be horizontal
+                //GS2.Log($"{preview?.condition} | {preview2?.condition}");
+                if (preview?.condition == EBuildCondition.JointCannotLift || preview2?.condition == EBuildCondition.JointCannotLift) // Make sure the error is that the endpoint must be horizontal
                 {
-                    preview.condition = EBuildCondition.Ok; // Ignore that endpoint horizontal error
+                    if (preview?.condition == EBuildCondition.JointCannotLift) preview.condition = EBuildCondition.Ok; // Ignore that endpoint horizontal error
+                    if (preview2?.condition == EBuildCondition.JointCannotLift) preview2.condition = EBuildCondition.Ok; // Ignore that endpoint horizontal error
                     for (int i = 0; i < count; i++) // Check the rest of the belt for errors
                     {
                         if ((__instance.buildPreviews[i].condition != EBuildCondition.Ok && __instance.buildPreviews[i].condition != EBuildCondition.JointCannotLift))
                         {
+                            //GS2.Warn("Some other error");
                             __result = false;
-                            return __result; //If there's some other problem with the belt, bail out.
+                            return __result; // If there's some other problem with the belt, bail out.
                         }
                     }
                     //___cursorText = "Click to build";
