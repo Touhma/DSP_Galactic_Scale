5a41e41f6b9559aaac59e855a97b1b8929fe0de6
kremnev8 <kremnev8super@yandex.ru>
2021-08-01 16:47:18 +0300

    Fix imports

--- GalacticScale2/Scripts/GalacticScale2.0/Patches/BuildTool_Click/CheckBuildConditions.cs
+++ GalacticScale2/Scripts/GalacticScale2.0/Patches/BuildTool_Click/CheckBuildConditions.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using HarmonyLib;
 using UnityEngine;
-using static GalacticScale.GS2;
 
 namespace GalacticScale
 {
