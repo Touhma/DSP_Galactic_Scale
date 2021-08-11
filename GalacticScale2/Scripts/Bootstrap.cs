36a4989705c6f070656a0312abce4abb518c2dc6
innominata <inno@xray.cat>
2021-08-11 21:24:59 +1200

    b84.1

--- GalacticScale2/Scripts/Bootstrap.cs
+++ GalacticScale2/Scripts/Bootstrap.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace GalacticScale
 {
-    [BepInPlugin("dsp.galactic-scale.2", "Galactic Scale 2 Plug-In", "2.0.84.0")]
+    [BepInPlugin("dsp.galactic-scale.2", "Galactic Scale 2 Plug-In", "2.0.84.1")]
     [BepInDependency("space.customizing.console", BepInDependency.DependencyFlags.SoftDependency)]
     public class Bootstrap : BaseUnityPlugin
     {
