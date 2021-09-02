b97676d16901526570de43f26f2bdb0440d9ed69
innominata <inno@xray.cat>
2021-09-03 00:14:35 +1200

    Update Debug.cs

--- GalacticScale2/Scripts/GalacticScale2.0/Patches/-Debug/Debug.cs
+++ GalacticScale2/Scripts/GalacticScale2.0/Patches/-Debug/Debug.cs
@@ -27,13 +27,98 @@ namespace GalacticScale
         //    }
         //    return true;
         //}
-        
-        
-       
-        
-        
+        [HarmonyPatch(typeof(UIReplicatorWindow), "OnPlusButtonClick")]
+        [HarmonyPrefix]
+        public static bool OnPlusButtonClick(ref UIReplicatorWindow __instance, int whatever)
+        {
+            GS2.Log("Test");
+            if (__instance.selectedRecipe != null)
+            {
+                if (!__instance.multipliers.ContainsKey(__instance.selectedRecipe.ID))
+                {
+                    __instance.multipliers[__instance.selectedRecipe.ID] = 1;
+                }
 
-        
-        
-    }
+                int num = __instance.multipliers[__instance.selectedRecipe.ID];
+                if (VFInput.shift) num += 10;
+                else if (VFInput.control) num += 100;
+                else num++;
+                if (num > 999)
+                {
+                    num = 1000;
+                }
+
+                __instance.multipliers[__instance.selectedRecipe.ID] = num;
+                __instance.multiValueText.text = num.ToString() + "x";
+            }
+
+            return false;
+        }
+
+        [HarmonyPatch(typeof(UIReplicatorWindow), "OnOkButtonClick")]
+        [HarmonyPrefix]
+        public static bool OnOkButtonClick(ref UIReplicatorWindow __instance, int whatever, bool button_enable)
+        {
+            GS2.Log("Test2");
+            if (__instance.selectedRecipe != null)
+            {
+                if (!__instance.selectedRecipe.Handcraft)
+                {
+                    UIRealtimeTip.Popup("该配方".Translate() + __instance.selectedRecipe.madeFromString + "生产".Translate(), true, 0);
+                    return false;
+                }
+
+                int id = __instance.selectedRecipe.ID;
+                if (!GameMain.history.RecipeUnlocked(id))
+                {
+                    UIRealtimeTip.Popup("配方未解锁".Translate(), true, 0);
+                    return false;
+                }
+
+                int num = 1;
+                if (__instance.multipliers.ContainsKey(id))
+                {
+                    num = __instance.multipliers[id];
+                }
+
+                if (num < 1)
+                {
+                    num = 1;
+                }
+                else if (num > 999)
+                {
+                    num = 1000;
+                }
+
+                int num2 = __instance.mechaForge.PredictTaskCount(__instance.selectedRecipe.ID, 99);
+                GS2.Log($"{num} - {num2}");
+                if (num > num2)
+                {
+                    num = num2;
+                }
+
+                if (num == 0)
+                {
+                    UIRealtimeTip.Popup("材料不足".Translate(), true, 0);
+                    return false;
+                }
+
+                if (__instance.mechaForge.AddTask(id, num) == null)
+                {
+                    UIRealtimeTip.Popup("材料不足".Translate(), true, 0);
+                    return false;
+                }
+
+                GameMain.history.RegFeatureKey(1000104);
+                
+            }return false;
+        }
+
+
+
+
+
+
+
+}
 }
