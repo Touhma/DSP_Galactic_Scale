d7b4575b2cf79362514b0aa9afc32b61dcc385d5
starfish <ac355deny@gmail.com>
2022-10-23 18:23:40 +0800

    Fix compat with game pass version

--- Scripts/Patches/UIVersionText/PatchOnUIVersionText.cs
+++ Scripts/Patches/UIVersionText/PatchOnUIVersionText.cs
@@ -18,7 +18,18 @@ namespace GalacticScale
         [HarmonyPatch(typeof(UIVersionText), "Refresh")]
         public static IEnumerable<CodeInstruction> Refresh_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            instructions = new CodeMatcher(instructions).MatchForward(true, new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "userName")).Advance(1).InsertAndAdvance(Transpilers.EmitDelegate<Func<string, string>>(text =>
+            var matcher = new CodeMatcher(instructions).MatchForward(true, new CodeMatch(i => i.opcode == OpCodes.Ldfld && ((FieldInfo)i.operand).Name == "userName"));
+            if (matcher.IsInvalid)
+            {
+                matcher.Start().MatchForward(true, new CodeMatch(i => i.opcode == OpCodes.Call && ((MethodInfo)i.operand).Name == "get_usernameAndSuffix"));
+            }
+            if (matcher.IsInvalid)
+            {
+                GS2.Warn("PatchOnUIVersionText.Refresh_Transpiler failed. GalacticScale mod version won't show up.");
+                return instructions;
+            }
+
+            instructions = matcher.Advance(1).InsertAndAdvance(Transpilers.EmitDelegate<Func<string, string>>(text =>
             {
                 text = $"Galactic Scale v {GS2.Version}\r\n{text}";
                 //if (!GS2.IsMenuDemo && GS2.Config.DebugMode) text += $"\r\n{GameMain.data.factories.Length} Factories\r\n{GameMain.data.warningSystem.tmpEntityPools.Length} tmpEntityPools";
