5e32820bac25efbea8fc79f91e3c00cd185c608c
innominata <inno@xray.cat>
2023-02-24 16:37:18 +1300

    messing around with chatGPT

--- Scripts/Patches/PlanetGrid/DetermineLongitudeSegmentCount.cs
+++ Scripts/Patches/PlanetGrid/DetermineLongitudeSegmentCount.cs
@@ -9,12 +9,10 @@ namespace GalacticScale
         [HarmonyPatch(typeof(PlanetGrid), "DetermineLongitudeSegmentCount")]
         public static bool DetermineLongitudeSegmentCount(PlanetGrid __instance, int latitudeIndex, int segment, ref int __result)
         {
-            //var a = 0;
             if (!DSPGame.IsMenuDemo)
                 if (!GS2.Vanilla)
                 {
                     if (!GS2.keyedLUTs.ContainsKey(segment))
-                        // GS2.Warn("Setting LUT");
                         GS2.SetLuts(segment, segment);
                     if (GS2.keyedLUTs.ContainsKey(segment))
                     {
@@ -23,11 +21,6 @@ namespace GalacticScale
                         if (index >= segment / 4) index = segment / 4 - index;
                         if (index < 0 || GS2.keyedLUTs[segment].Length == 0)
                         {
-                            // GS2.Warn($"Using DSLC {latitudeIndex} {segment} {__result} {GS2.keyedLUTs[segment].Length} Index:{index}");
-                            // GS2.Warn($"Index:{index}");
-                            // GS2.WarnJson(GS2.keyedLUTs[segment]);
-                            // var index2 = Mathf.CeilToInt(Mathf.Abs(Mathf.Cos((float)(latitudeIndex / (double)(segment / 4f) * 3.14159274101257 * 0.5))) * segment);
-                            // __result = index2 < 500 ? PlatformSystem.segmentTable[index2] : (index2 + 49) / 100 * 100;
                             __result = 4;
                             return false;
                         }
@@ -44,8 +37,6 @@ namespace GalacticScale
                         __result = index < 500 ? PlatformSystem.segmentTable[index] : (index + 49) / 100 * 100;
                         //a = 2;
                     }
-
-                    //if (a + latitudeIndex == 34874399) return true;
                     if (__result < 4) __result = 4;
                     return false;
                 }
--- Scripts/Patches/PlanetRawData/QueryModifiedHeight.cs
+++ Scripts/Patches/PlanetRawData/QueryModifiedHeight.cs
@@ -62,3 +62,78 @@ namespace GalacticScale
         }
     }
 }
+
+/* ChatGPT Generated Rewrite
+
+public static bool QueryModifiedHeight(ref PlanetRawData planetData, ref float height, Vector3 position)
+{
+    // Normalize input position vector
+    position.Normalize();
+    
+    // Get index of vertex in the planet data
+    var vertexIndex = planetData.indexMap[planetData.PositionHash(position)];
+    
+    // Calculate precision-related values
+    var radiusPrecision = 3.1415927f / (planetData.precision * 2) * 1.2f;
+    var radiusPrecisionSq = radiusPrecision * radiusPrecision;
+    
+    // Initialize variables for magnitude and height calculations
+    var magnitudeOnPrecisionSum = 0f;
+    var heightTimePrecisionSum = 0f;
+    var stride = planetData.stride;
+    
+    // Iterate over neighboring vertices to calculate final height
+    for (var x = -1; x <= 3; x++)
+    {
+        for (var y = -1; y <= 3; y++)
+        {
+            var neighborIndex = vertexIndex + x + y * stride;
+            if ((ulong)neighborIndex < (ulong)planetData.dataLength)
+            {
+                var sqrMagnitude = (planetData.vertices[neighborIndex] - position).sqrMagnitude;
+                if (sqrMagnitude <= radiusPrecisionSq)
+                {
+                    // Calculate magnitude on precision and multiply by factored scale
+                    var magnitudeOnPrecision = 1f - Mathf.Sqrt(sqrMagnitude) / radiusPrecision;
+                    magnitudeOnPrecision *= planetData.GetFactoredScale();
+                    
+                    // Get modification level of vertex
+                    var modLevel = planetData.GetModLevel(neighborIndex);
+                    
+                    // If vertex has modifications, adjust height calculation accordingly
+                    var heightData = planetData.heightData[neighborIndex];
+                    if (modLevel > 0)
+                    {
+                        var modPlane = planetData.GetModPlaneInt(neighborIndex);
+                        if (modLevel == 3)
+                        {
+                            heightData = modPlane;
+                        }
+                        else
+                        {
+                            var blendFactor = modLevel * 0.3333333f;
+                            heightData = planetData.heightData[neighborIndex] * (1f - blendFactor) + modPlane * blendFactor;
+                        }
+                    }
+                    
+                    // Add to magnitude and height sums
+                    magnitudeOnPrecisionSum += magnitudeOnPrecision;
+                    heightTimePrecisionSum += heightData * magnitudeOnPrecision;
+                }
+            }
+        }
+    }
+
+    // Handle cases where no valid vertices were found
+    if (magnitudeOnPrecisionSum == 0f)
+    {
+        GS2.Log($"Bad Query. LocalPlanet:{GameMain.localPlanet.name} Vector:{position}");
+        height = planetData.heightData[0] * 0.01f;
+        return false;
+    }
+
+    // Calculate final height using magnitude and height sums
+    height = heightTimePrecisionSum / magnitudeOnPrecisionSum * 0.01f;
+    return false;
+}
+*/
