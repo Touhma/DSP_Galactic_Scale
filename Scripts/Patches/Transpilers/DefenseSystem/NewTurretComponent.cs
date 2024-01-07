d5d0794bc774ce3deecad94e133bf3bdc0465fe2
innominata <inno@xray.cat>
2024-01-07 14:35:21 +1300

    Update NewTurretComponent.cs

--- Scripts/Patches/Transpilers/DefenseSystem/NewTurretComponent.cs
+++ Scripts/Patches/Transpilers/DefenseSystem/NewTurretComponent.cs
@@ -37,34 +37,6 @@ namespace GalacticScale
         
                  return instructions;
         }
-        [HarmonyTranspiler]
-        [HarmonyPatch(typeof(DefenseSystem),  nameof(DefenseSystem.AfterTurretsImport))]
-        public static IEnumerable<CodeInstruction> AfterTurretsImportTranspiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var matcher = new CodeMatcher(instructions);
-            matcher.MatchForward(
-                true,
-                new CodeMatch(i => i.opcode == Ldarg_0),
-                new CodeMatch(i => i.opcode == Ldfld && i.operand.ToString().Contains("TurretComponent")),
-                new CodeMatch(i => i.opcode == Callvirt),
-                new CodeMatch(i => i.opcode == Stloc_0)
-            );
-            if (!matcher.IsInvalid)
-            {
-                matcher.Advance(1)
-                    .InsertAndAdvance(new CodeInstruction(Ldarg_0))
-                    .InsertAndAdvance(new CodeInstruction(Ldloc_0))
-                    .InsertAndAdvance(new CodeInstruction(Call,
-                        AccessTools.Method(typeof(TurretComponentTranspiler),
-                            nameof(TurretComponentTranspiler.AddTurret)))
-                    );
-                instructions = matcher.InstructionEnumeration();
-                return instructions;
-            }
-
-            Bootstrap.Logger.LogInfo("Transpiler failed!  ");
-        
-            return instructions;
-        }
+      
     }
 }
